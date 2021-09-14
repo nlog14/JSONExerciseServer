24301e0da0b99bc6be1a02beb79ca30f659586e1
Nohely <ngedeon14@yahoo.com>
2021-09-14 11:00:27 +0200

    Add project files.

---
+++ JSONExerciseServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+using JsonCarExercise;
+
+namespace JSONExerciseServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the server");
+
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 10002);
+           
+            listener.Start();
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Incoming client");
+                Task.Run((() => { HandleClient(socket); }));
+
+            }
+
+        }
+
+        private static void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            string message = reader.ReadLine();
+
+            Car FromJsonCar = JsonSerializer.Deserialize<Car>(message);
+
+            Console.WriteLine("Client said:" + FromJsonCar.Color + FromJsonCar.Model + FromJsonCar.registrationNumber);
+
+            writer.WriteLine("Car received");
+            writer.Flush();
+            socket.Close();
+        }
+    }
+}
